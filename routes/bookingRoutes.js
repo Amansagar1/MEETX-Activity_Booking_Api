57c21ba47d0c51aff1f79901f09a6d2d1a5effe4
Sagar Sharma <kumaramansagar01@gmail.com>
2025-05-09 16:53:32 +0530

    api fix

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const router = express.Router();
 const { protect } = require('../middlewares/auth');
 const { bookActivity, getMyBookings } = require('../controllers/bookingController');
-const { bookingValidation } = require('../validations/bookingValidations');
-const validate = require('../middlewares/validate');
 
-router.route('/').post(protect, validate(bookingValidation), bookActivity);
-router.route('/mybookings').get(protect, getMyBookings);
+const router = express.Router();
+
+// Protect these routes with the `protect` middleware
+router.post('/', protect, bookActivity);
+router.get('/mybookings', protect, getMyBookings);
 
 module.exports = router;
